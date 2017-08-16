8502d08509fc7c1e79e8152d94ae03d20774f7b1
lp <lp7725210@163.com>
2017-08-16 17:19:19 +0800

    新增WGS84，GCJ02， BD09坐标转换 （包含百度地图坐标转谷歌坐标）

---
+++ src/main/java/com/bean/Gps.java
@@ -0,0 +1,37 @@
+package com.bean;
+
+/**
+ * Created by lvping on 2017/8/16.
+ */
+public class Gps {
+
+        private double wgLat;
+        private double wgLon;
+
+        public Gps(double wgLat, double wgLon) {
+            setWgLat(wgLat);
+            setWgLon(wgLon);
+        }
+
+        public double getWgLat() {
+            return wgLat;
+        }
+
+        public void setWgLat(double wgLat) {
+            this.wgLat = wgLat;
+        }
+
+        public double getWgLon() {
+            return wgLon;
+        }
+
+        public void setWgLon(double wgLon) {
+            this.wgLon = wgLon;
+        }
+
+        @Override
+        public String toString() {
+            return wgLat + "," + wgLon;
+        }
+
+}
